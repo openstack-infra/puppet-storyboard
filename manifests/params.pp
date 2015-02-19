6108328b88ab7266558127411d0baf4b97d73a4a
Michael Krotscheck <krotscheck@gmail.com>
2015-02-19 15:20:13 -0800

    Moved all module parameters into one manifest.
    All configuration now happens in storyboard::params. Modules
    (other than the init module) no longer contain their own params,
    and pull them in.
    
    Change-Id: I1a3738fa6689551b59e82e37f5553546517a9586

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,21 +16,94 @@
 #
 # Centralized configuration management for the storyboard module.
 #
-class storyboard::params () {
+class storyboard::params (
 
-  include apache::params
+  # Working and Install directories
+  $src_root_api           = '/opt/storyboard',
+  $src_root_webclient     = '/opt/storyboard-webclient',
+  $install_root_api       = '/var/lib/storyboard',
+  $install_root_webclient = '/var/lib/storyboard/www',
 
-  $user = $apache::params::user
-  $group = $apache::params::group
+  # The user under which storyboard will run.
+  $user                   = $apache::params::user,
+  $group                  = $apache::params::group,
+  $server_admin           = undef,
+  $hostname               = $::ipaddress,
 
+  # [default] storyboard.conf
+  $working_root           = '/var/lib/storyboard/spool',
+  $enable_notifications   = true,
+
+  # [oauth] storyboard.conf
+  $openid_url             = 'https://login.launchpad.net/+openid',
+  $authorization_code_ttl = 300,
+  $access_token_ttl       = 3600,
+  $refresh_token_ttl      = 604800,
+  $valid_oauth_clients    = ['storyboard.openstack.org'],
+
+  # [cron] storyboard.conf
+  $enable_cron            = true,
+
+  # [cors] storyboard.conf
+  $cors_allowed_origins   = [],
+  $cors_max_age           = 3600,
+
+  # [database] storyboard.conf
+  $mysql_user             = 'storyboard',
+  $mysql_user_password,
+  $mysql_host             = localhost,
+  $mysql_port             = 3306,
+  $mysql_database         = 'storyboard',
+
+  # [notifications] storyboard.conf
+  $rabbitmq_host          = 'localhost',
+  $rabbitmq_port          = 5672,
+  $rabbitmq_vhost         = '/',
+  $rabbitmq_user          = 'storyboard',
+  $rabbitmq_user_password,
+
+  # [plugin_token_cleaner] storyboard.conf
+  $enable_token_cleanup   = 'True',
+
+  # StoryBoard Worker configuration
+  $worker_count           = 5,
+  $worker_use_upstart     = false,
+
+  # Apache2 ssl configuration
+  $ssl_cert_content = undef,
+  $ssl_cert         = '/etc/ssl/certs/storyboard.pem',
+  $ssl_key_content  = undef,
+  $ssl_key          = '/etc/ssl/private/storyboard.key',
+  $ssl_ca_content   = undef,
+  $ssl_ca           = undef, # '/etc/ssl/certs/ca.pem'
+) inherits apache::params {
+
+  # Download source
+  $webclient_filename     = 'storyboard-webclient-latest.tar.gz'
+  $webclient_url          = "http://tarballs.openstack.org/storyboard-webclient/${webclient_filename}"
+
+  # Build the connection string from individual parameters
+  $mysql_connection_string = "mysql+pymysql://${mysql_user}:${mysql_user_password}@${mysql_host}:${mysql_port}/${mysql_database}"
+
+  # CA file needs special treatment, since we want the path variable
+  # to be undef in some cases.
+  if $ssl_ca == undef and $ssl_ca_content != undef {
+    $resolved_ssl_ca = '/etc/ssl/certs/storyboard.ca.pem'
+  } else {
+    $resolved_ssl_ca = $ssl_ca
+  }
+
+  # Resolve a few parameters based on the install environment.
   case $::osfamily {
     'Debian': {
       if $::operatingsystem == 'Ubuntu' and $::operatingsystemrelease >= 13.10 {
         $apache_version = '2.4'
         $manage_rabbit_repo = false
+        $new_vhost_perms = true
       } else {
         $apache_version = '2.2'
         $manage_rabbit_repo = true
+        $new_vhost_perms = false
       }
     }
     default: {
