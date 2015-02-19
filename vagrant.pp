1bdbcbe0d79cbc72e695f6c3498fb41b4120e956
Michael Krotscheck <krotscheck@gmail.com>
2015-02-19 09:49:22 -0800

    Added new OAuth Parameters
    Storyboard.conf now accepts authorization_code_ttl and a list of
    valid_oauth_clients.
    
    Change-Id: I29495a0b640c3ca097cca8c17349df5cc42388de

--- vagrant.pp
+++ vagrant.pp
@@ -3,6 +3,10 @@ node 'puppet-storyboard-precise64' {
     mysql_user_password    => 'storyboard',
     rabbitmq_user_password => 'storyboard',
     hostname               => '192.168.99.22',
+
+    valid_oauth_clients    => [
+      '192.168.99.22'
+    ],
   }
 }
 
@@ -11,5 +15,9 @@ node 'puppet-storyboard-trusty64' {
     mysql_user_password    => 'storyboard',
     rabbitmq_user_password => 'storyboard',
     hostname               => '192.168.99.23',
+
+    valid_oauth_clients    => [
+      '192.168.99.23'
+    ],
   }
 }
