57d35187187f1af476889da773d778e7781f865c
Michael Krotscheck <krotscheck@gmail.com>
2015-02-19 09:49:22 -0800

    Added new OAuth Parameters
    Storyboard.conf now accepts authorization_code_ttl and a list of
    valid_oauth_clients.
    
    Change-Id: I29495a0b640c3ca097cca8c17349df5cc42388de

--- vagrant.pp
+++ vagrant.pp
@@ -3,5 +3,10 @@ node default {
     mysql_user_password    => 'storyboard',
     rabbitmq_user_password => 'storyboard',
     hostname               => '192.168.99.22',
+
+    valid_oauth_clients    => [
+      'storyboard.openstack.org',
+      '192.168.99.22'
+    ],
   }
 }
