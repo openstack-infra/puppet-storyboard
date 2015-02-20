0cc79be27d117f56a9e129df19ca0409cda6ca35
Michael Krotscheck <krotscheck@gmail.com>
2015-02-20 13:23:30 -0800

    Added precise vagrant box
    This adds a precise vagrant box for testing the puppet module. You
    may now use 'vagrant up precise' or 'vagrant up trusty' to test
    this module.
    
    Change-Id: I875a14db363b00bddf8886eb5d05d2c35a22baf5

--- vagrant.pp
+++ vagrant.pp
@@ -1,7 +1,14 @@
 node default {
+
+  if $::operatingsystem == 'Ubuntu' and $::operatingsystemrelease >= 13.10 {
+    $hostname = '192.168.99.23'
+  } else {
+    $hostname = '192.168.99.22'
+  }
+
   class { 'storyboard':
     mysql_user_password    => 'storyboard',
     rabbitmq_user_password => 'storyboard',
-    hostname               => '192.168.99.22',
+    hostname               => $hostname,
   }
 }
--- vagrant.sh
+++ vagrant.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Install Puppet!
+if [ ! -f /etc/apt/sources.list.d/puppetlabs.list ]; then
+  lsbdistcodename=`lsb_release -c -s`
+  wget https://apt.puppetlabs.com/puppetlabs-release-${lsbdistcodename}.deb
+  sudo dpkg -i puppetlabs-release-${lsbdistcodename}.deb
+  sudo apt-get update
+  sudo apt-get dist-upgrade -y
+fi
+
 if [ ! -d /etc/puppet/modules/stdlib ]; then
   puppet module install puppetlabs-stdlib --version 3.2.0
 fi
